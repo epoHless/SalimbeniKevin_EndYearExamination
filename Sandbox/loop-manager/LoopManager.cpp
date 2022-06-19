0eccf257336674ef8a563b26da47fd63947722dd
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2022-06-19 19:46:14 +0200

    Added required functions in order to render a window
    Added:
    - createWindow
    - setMaxFPS
    - getFPS
    - fpsLimitToggler
    - run

--- Sandbox/loop-manager/LoopManager.cpp
+++ Sandbox/loop-manager/LoopManager.cpp
@@ -1,25 +1,98 @@
 #include "LoopManager.h"
 
-LoopManager::LoopManager()
-{
-}
 
 LoopManager::~LoopManager()
 {
+	delete gameWindow;
+}
+
+void LoopManager::createWindow(const float width, const float height, const char* title)
+{
+	gameWindow = new sf::RenderWindow(sf::VideoMode(width, height), title);
+}
+
+void LoopManager::setMaxFPS(unsigned newLimit)
+{
+	maxFPS = newLimit;
+}
+
+unsigned LoopManager::getFPS() const
+{
+	return 1 / elapsedTime;
+}
+
+void LoopManager::fpsLimitToggler()
+{
+	isFpsLimited = !isFpsLimited;
+}
+
+void LoopManager::run()
+{
+	lastTime = timeManager.GetCurrentTime();
+	while (gameWindow->isOpen())
+	{
+		updateGameTime();
+		pollEvents();
+		update();
+		draw();
+
+		if (isFpsLimited)
+		{
+			sf::sleep(sf::seconds(1 / maxFPS));
+		}
+	}
 }
 
 void LoopManager::initialize()
 {
+	/* To be filled with
+	
+		add gameobjects to the game
+
+	*/
 }
 
 void LoopManager::draw()
 {
+	gameWindow->clear();
+
+	/* To be filled with
+	
+		for all the entities in game
+			get entity renderer
+			draw the renderer	
+	*/
+
+	gameWindow->display();
 }
 
 void LoopManager::update()
 {
+	/* To be filled with
+
+		for all the gameobjects in the game
+			if is active and enabled
+				update it
+
+	*/
 }
 
 void LoopManager::pollEvents()
 {
+	sf::Event events{};
+	while (gameWindow->pollEvent(events))
+	{
+		if (events.type == sf::Event::EventType::Closed)
+		{
+			gameWindow->close();
+		}
+	}
+}
+
+void LoopManager::updateGameTime()
+{
+	currentTime = timeManager.GetCurrentTime();
+	elapsedTime = TimeManager::GetElapsedTime(currentTime.asSeconds(), lastTime.asSeconds());
+	lag += elapsedTime;
+	lastTime = currentTime;
 }
--- Sandbox/loop-manager/LoopManager.h
+++ Sandbox/loop-manager/LoopManager.h
@@ -6,25 +6,66 @@
 
 class LoopManager
 {
+//Constructors / Destructor
 public:
 	/// <summary>
 	/// Default Loop Manager Contructor
 	/// </summary>
-	LoopManager();	
+	LoopManager() : gameWindow(nullptr){};
 	/// <summary>
 	/// Default Loop Manager Destructor
 	/// </summary>
 	~LoopManager();
 
+//Public Functions
+public:
+
+	/// <summary>
+	/// Creates the game window
+	/// </summary>
+	void createWindow(const float width, const float height, const char* title);
+
+	/// <summary>
+	/// Change the amount of max FPS for the game window (standard is 60 FPS)
+	/// </summary>
+	/// <param name="newLimit">The new FPS limit</param>
+	void setMaxFPS(unsigned newLimit);
+
+	/// <summary>
+	/// Returns the current frame's FPS
+	/// </summary>
+	/// <returns></returns>
+	unsigned getFPS()const;
+
+	/// <summary>
+	/// Toggle the FPS limit ON or OFF depending on the current setting
+	/// </summary>
+	void fpsLimitToggler();
+
+	/// <summary>
+	/// Main loop function
+	/// </summary>
+	void run();
+
+
+//Private Functions
 private:
 	void initialize();
 	void draw();
 	void update();
 	void pollEvents();
+	void updateGameTime();
 
+//Private Members
 private:
+	sf::RenderWindow* gameWindow;
+
+	unsigned maxFPS = 60;
+	bool isFpsLimited = true;
+
 	TimeManager timeManager;
-	sf::RenderWindow gameWindow;
+	float elapsedTime;
+	float lag;
 	sf::Time lastTime, currentTime;
 };
 
