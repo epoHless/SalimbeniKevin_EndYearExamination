321f25fe086cf5552b220338998f4aa94a5f1eb4
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2022-06-19 17:56:35 +0200

    Defined Time Manager structure and functions

--- Sandbox/time-manager/TimeManager.cpp
+++ Sandbox/time-manager/TimeManager.cpp
@@ -1,1 +1,15 @@
 #include "TimeManager.h"
+
+TimeManager::TimeManager(){}
+
+TimeManager::~TimeManager(){}
+
+sf::Time TimeManager::GetCurrentTime() const
+{
+	return clock.getElapsedTime();
+}
+
+float TimeManager::GetElapsedTime(float currentTime, float lastTime)
+{
+	return currentTime - lastTime;
+}
--- Sandbox/time-manager/TimeManager.h
+++ Sandbox/time-manager/TimeManager.h
@@ -1,5 +1,21 @@
-#pragma once
+#ifndef TIME_MANAGER_H
+#define TIME_MANAGER_H
+
+#include "SFML/System.hpp"
+
 class TimeManager
 {
+public:
+
+	TimeManager();
+	virtual ~TimeManager();
+
+	sf::Time GetCurrentTime()const;
+
+	static float GetElapsedTime(float currentTime, float lastTime);
+
+private:
+	sf::Clock clock;
 };
 
+#endif 
