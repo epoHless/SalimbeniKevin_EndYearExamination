e446ac58edd641bce1291934e0c33da290f04d6c
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2022-06-20 20:27:56 +0200

    Added ITickable interface

---
+++ WTGD/src/interfaces/ITickable.h
@@ -0,0 +1,43 @@
+#ifndef ITICKABLE_H
+#define ITICKABLE_H
+
+#ifdef DLL_EXPORT
+#define ITICKABLE_API __declspec(dllexport)
+#else
+#define ITICKABLE_API __declspec(dllimport)
+#endif
+
+namespace WTGD
+{
+
+	class ITICKABLE_H ITickable
+	{
+	public:
+		/// <summary>
+		/// Check if the tick is enabled
+		/// </summary>
+		/// <returns>Returns true if it is enabled, false otherswise</returns>
+		bool is_tick_enabled()const { return tickable; }
+		/// <summary>
+		/// Enables the tick
+		/// </summary>
+		void enable_tick() { tickable = true; }
+		/// <summary>
+		/// Disables the tick
+		/// </summary>
+		void disable_tick() { tickable = false; }
+
+	protected:
+		/// <summary>
+		/// Called every frame
+		/// </summary>
+		/// <param name="delta"></param>
+		virtual void on_update(const float delta) = 0;
+
+	private:
+		bool tickable = true;
+	};
+
+}
+
+#endif ITICKABLE_H
