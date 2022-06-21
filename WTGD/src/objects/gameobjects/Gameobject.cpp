32bb0b1aa52b47051ebf1b40357487ba4804e52b
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2022-06-21 19:41:46 +0200

    Added gameobject WIP, and Component base class

--- WTGD/src/objects/gameobjects/Gameobject.cpp
+++ WTGD/src/objects/gameobjects/Gameobject.cpp
@@ -1,1 +1,33 @@
 #include "Gameobject.h"
+
+WTGD::GameObject::GameObject(const std::string name, const bool is_active) : Object(name, is_active)
+{
+	this->name = name;
+	this->is_active = is_active;
+}
+
+WTGD::GameObject::GameObject(const std::string name, const std::vector<Component*> components, const bool is_active) : Object(name, is_active)
+{
+	this->name = name;
+	this->components = components;
+	this->is_active = is_active;
+}
+
+void WTGD::GameObject::add_component(Component* component)
+{
+	component->owner = this;
+	components.push_back(component);
+}
+
+void WTGD::GameObject::remove_component(Component* component)
+{
+	components.erase(std::remove(components.begin(), components.end(), component), components.end());
+}
+
+void WTGD::GameObject::on_update(const float delta_time)
+{
+	for (auto item : components)
+	{
+		if (item->is_active && item->tick_enabled()) { item->on_update(delta_time); }
+	}
+}
--- WTGD/src/objects/gameobjects/Gameobject.h
+++ WTGD/src/objects/gameobjects/Gameobject.h
@@ -8,13 +8,61 @@
 #endif
 
 #include <string>
+#include <vector>
+
+#include "../Object.h"
+#include "../../interfaces/ITickable.h"
+
+class Component;
 
 namespace WTGD 
 {
-	class GAMEOBJECT_API GameObject
+	class GAMEOBJECT_API GameObject : public Object, public ITickable
 	{
 	public:
-		std::string name;
+		GameObject(const std::string name, const bool active = true);
+		GameObject(const std::string name, const std::vector<Component*> components,const bool active = true);
+		~GameObject() = default;
+
+		void add_component(Component* component);
+		void remove_component(Component* component) {};
+
+		template<class T, class = Component> T* get_component()const
+		{
+			if (components.size() == 0) return nullptr;
+
+			for (Component* comp : components)
+			{
+				const auto val = dynamic_cast<T*>(comp);
+				if (!val) continue;
+
+				return val;
+			}
+			return nullptr;
+		}
+
+		template<class T, class = Component> std::vector<T*> get_components()const
+		{
+			std::vector<T*> output;
+
+			if (components.size() == 0) return nullptr;
+
+			for each (Component * comp in this->components)
+			{
+				const auto val = dynamic_cast<T*>(comp);
+				if (!val) continue;
+
+				output.push_back(val);
+			}
+			return output;
+		}
+
+	protected:
+		// Inherited via ITickable
+		virtual void on_update(const float delta) override;
+
+	private:
+		std::vector<Component*> components;
 
 	};
 }
